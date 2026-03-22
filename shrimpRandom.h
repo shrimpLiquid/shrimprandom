a2e6d58e2829a1b23466395b56df21f3b7eb9c95
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2026-03-22 11:35:58 -0700

    Update shrimpRandom.h

--- shrimpRandom.h
+++ shrimpRandom.h
@@ -5,7 +5,16 @@
 
 class shrimpRandom {
   public:
-    static unsigned long generate(unsigned long howsmall, unsigned long howbig, int pin, int avg);
+    static unsigned long generate(unsigned long howsmall, unsigned long howbig, int pin, int avg) {
+        unsigned long value = 0;
+        for (int i = 0; i <= 63; i++) {
+            int bit = analogRead(pin) / avg;
+            value *= 2;
+            value += bit;
+        }
+        value = (value % ((howbig + 1) - howsmall) + howsmall);
+        return value;
+    }
 };
 
 #endif
